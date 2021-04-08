7c382a8f9b35078682b81ee72d5b8843f1f8adf1
Emin Shamshoian <emin.shamshoianmalhami.599@my.csun.edu>
2021-04-08 02:31:50 +0000

    Delete helloworld.java

--- helloworld.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World from Emin Shamshoian");
-		System.out.println("Hello World from Joseph Clifford");
-		System.out.println("Hello World from Alexis");
-	}
-}
